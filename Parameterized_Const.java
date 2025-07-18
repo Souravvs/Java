23a9d32027da3279424f2b2763ea9a958e1b31a1
Sourav V S <souravvs2028@cs.sjcetpalai.ac.in>
2025-07-18 10:52:11 +0530

    Add files via upload

---
+++ Parameterized_Const.java
@@ -0,0 +1,39 @@
+package project1;
+
+import java.util.Scanner;
+
+class Student{
+	String name;
+	int rollNum;
+	String department;
+	
+	Student(String name, int rollNum, String department){
+		this.name = name;
+		this.rollNum = rollNum;
+		this.department = department;
+	}
+	public void printStudentInfo() {
+		System.out.println("Student Details:");
+		System.out.println("Name:\t\t" + name);
+		System.out.println("Roll Number:\t" + rollNum);
+		System.out.println("Department:\t" + department);
+	}
+}
+
+public class Parameterized_Const {
+	public static void main(String[] args) {
+		String name;
+		int rollNum;
+		String dept;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name: ");
+		name = sc.nextLine();
+		System.out.println("Enter the roll number: ");
+		rollNum = sc.nextLine();
+		System.out.println("Enter the department: ");
+		dept = sc.nextLine();
+		
+		Student stud = new Student(name, rollNum, dept);
+		stud.printStudentInfo();
+	}
+}
