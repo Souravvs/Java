d19b0ee68a96d9f7b1a3a727b6522b742f0b29ac
Sourav V S <souravvs2028@cs.sjcetpalai.ac.in>
2025-08-08 10:19:42 +0530

    Create Extended_Book.java

---
+++ Extended_Book.java
@@ -0,0 +1,59 @@
+class Book{
+	String title;
+	String author;
+	double price;
+
+	Book(String title, String author, double price){
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+	
+	public void displayDetails(){
+		System.out.println("Title: " + title);
+		System.out.println("Author: " + author);
+		System.out.println("Price: " + price);
+	}
+}
+
+class EBook extends Book{
+	Double fileSize;
+	
+	EBook(String title, String author, double price, double fileSize){
+		super(title, author, price);
+		this.fileSize = fileSize;
+	}
+	
+	public void displayDetails() {
+		super.displayDetails();
+		System.out.println("File Size: " + fileSize + "MB");
+	}
+}
+
+class PrintedBook extends Book{
+	int numberOfPages;
+	
+	PrintedBook(String title, String author, double price, int numberOfPages){
+		super(title, author, price);
+		this.numberOfPages = numberOfPages;
+	}
+	
+	public void displayDetails() {
+		System.out.println("\n");
+		super.displayDetails();
+		System.out.println("Pages: " + numberOfPages);
+	}
+	
+}
+
+public class Extended_Class {
+	public static void main(String[] args) {
+		Book book = new Book("Crime and Punishment", "Fyodor Dostoevsky", 269.00);
+		
+		EBook ebook = new EBook("Crime and Punishment", "Fyodor Dostoevsky", 269.00, 2.39);
+		ebook.displayDetails();
+		
+		PrintedBook printedbook = new PrintedBook("Crime and Punishment", "Fyodor Dostoevsky", 269.00, 488);
+		printedbook.displayDetails();
+	}
+}
